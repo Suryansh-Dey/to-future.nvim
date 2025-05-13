02ba9644a0ca5b9b026262ed3a5b34aac81394b9
Suryansh-Dey <deysuryansh@gmail.com>
2025-05-13 09:31:35 +0530

    Using colors to represent distance

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # setup in lazy
 ```lua
-vim.api.nvim_set_hl(0, "to-future-hi", { bg = "#005555", fg = "#ffffff", bold = true })
+vim.api.nvim_set_hl(0, "to-future-hi-1", { bg = "#ffffff", fg = "#000000", bold = true })
+vim.api.nvim_set_hl(0, "to-future-hi-2", { bg = "#ffff00", fg = "#000000", bold = true })
+vim.api.nvim_set_hl(0, "to-future-hi-3", { bg = "#ff8800", fg = "#000000", bold = true })
+vim.api.nvim_set_hl(0, "to-future-hi-4", { bg = "#ff0000", fg = "#000000", bold = true })
 return {
-   "suryansh-dey/to-future.nvim",
-   event = "VeryLazy",
-   opts = { all_hl = "Comment", char_hl = "to-future-hi" },
+    "suryansh-dey/to-future.nvim",
+    event = "VeryLazy",
+    opts = { all_hl = { "to-future-hi-1", "to-future-hi-2", "to-future-hi-3", "to-future-hi-4", "Comment" } },
 }
 ```
--- lua/init.lua
+++ lua/init.lua
@@ -1,18 +1,12 @@
 local ns_id = vim.api.nvim_create_namespace("to-future_line_highlight_ns")
-local function highlight_char(bufnr, target, hl)
+local function highlight_char(bufnr, matches, hl)
     vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
     local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
-    row = row - 1
-    local line = vim.api.nvim_buf_get_lines(bufnr, row, row + 1, false)[1]
-
-    for i = 0, #line do
-        local char = line:sub(i, i)
-        if char == target then
-            vim.api.nvim_buf_set_extmark(bufnr, ns_id, row, i - 1, {
-                end_col = i,
-                hl_group = hl
-            })
-        end
+    for i = 2, #matches do
+        vim.api.nvim_buf_set_extmark(bufnr, ns_id, row - 1, matches[i], {
+            end_col = matches[i] + 1,
+            hl_group = hl[math.min(i - 1, #hl)]
+        })
     end
 
     local temp = vim.api.nvim_create_namespace("to-future-temp")
@@ -33,14 +27,21 @@ local function highlight_forward(bufnr, hl)
     local matches = {}
     for i = col + 2, #line do
         local char = line:sub(i, i)
-        if matches[char] and char ~= ' ' then
+        if char == ' ' then
+            goto continue
+        end
+        if matches[char] then
             vim.api.nvim_buf_set_extmark(bufnr, ns_id, row, i - 1, {
                 end_col = i,
-                hl_group = hl
+                hl_group = hl[math.min(#matches[char], #hl)]
             })
+            table.insert(matches[char], i - 1)
+        else
+            matches[char] = { i - 1 }
         end
-        matches[char] = true
+        ::continue::
     end
+    return matches
 end
 local function highlight_backward(bufnr, hl)
     local row, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -50,38 +51,53 @@ local function highlight_backward(bufnr, hl)
     local matches = {}
     for i = col, 0, -1 do
         local char = line:sub(i, i)
-        if matches[char] and char ~= ' ' then
+        if char == ' ' then
+            goto continue
+        end
+        if matches[char] then
             vim.api.nvim_buf_set_extmark(bufnr, ns_id, row, i - 1, {
                 end_col = i,
-                hl_group = hl
+                hl_group = hl[math.min(#matches[char], #hl)]
             })
+            table.insert(matches[char], i - 1)
+        else
+            matches[char] = { i - 1 }
         end
-        matches[char] = true
+        ::continue::
     end
+    return matches
 end
-local function start_highlight_forward(all_hl, char_hl)
+local function start_highlight_forward(all_hl)
     local bufnr = vim.api.nvim_get_current_buf()
-    highlight_forward(bufnr, all_hl)
+    local matches = highlight_forward(bufnr, all_hl)
     vim.cmd('redraw')
     local done = false
     local temp = vim.api.nvim_create_namespace("to-future-temp2")
     vim.on_key(function(key)
         if done then
-            highlight_char(bufnr, key, char_hl)
+            if matches[key] == nil then
+                vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
+            else
+                highlight_char(bufnr, matches[key], all_hl)
+            end
             vim.on_key(nil, temp)
         end
         done = true
     end, temp)
 end
-local function start_highlight_backward(all_hl, char_hl)
+local function start_highlight_backward(all_hl)
     local bufnr = vim.api.nvim_get_current_buf()
-    highlight_backward(bufnr, all_hl)
+    local matches = highlight_backward(bufnr, all_hl)
     vim.cmd('redraw')
     local done = false
     local temp = vim.api.nvim_create_namespace("to-future-temp2")
     vim.on_key(function(key)
         if done then
-            highlight_char(bufnr, key, char_hl)
+            if matches[key] == nil then
+                vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
+            else
+                highlight_char(bufnr, matches[key], all_hl)
+            end
             vim.on_key(nil, temp)
         end
         done = true
@@ -90,19 +106,19 @@ end
 return {
     setup = function(opts)
         vim.keymap.set({ 'n', 'v', 'o', 'x' }, 'f', function()
-            start_highlight_forward(opts.all_hl, opts.char_hl)
+            start_highlight_forward(opts.all_hl)
             return 'f'
         end, { expr = true })
         vim.keymap.set({ 'n', 'v', 'o', 'x' }, 't', function()
-            start_highlight_forward(opts.all_hl, opts.char_hl)
+            start_highlight_forward(opts.all_hl)
             return 't'
         end, { expr = true })
         vim.keymap.set({ 'n', 'v', 'o', 'x' }, 'F', function()
-            start_highlight_backward(opts.all_hl, opts.char_hl)
+            start_highlight_backward(opts.all_hl)
             return 'F'
         end, { expr = true })
         vim.keymap.set({ 'n', 'v', 'o', 'x' }, 'T', function()
-            start_highlight_backward(opts.all_hl, opts.char_hl)
+            start_highlight_backward(opts.all_hl)
             return 'T'
         end, { expr = true })
     end
