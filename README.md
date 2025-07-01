f12c3aa109993857168f989c58db1c6ffac8c92a
Suryansh-Dey <deysuryansh@gmail.com>
2025-07-01 06:41:19 +0530

    Removing gray from defaults

--- README.md
+++ README.md
@@ -15,13 +15,13 @@ return {
 ```
 ## Defaults
 ```lua
-vim.api.nvim_set_hl(0, "to-future-hi-1", { bg = "#777777", fg = "#000000", bold = true })
-vim.api.nvim_set_hl(0, "to-future-hi-2", { bg = "#ffff00", fg = "#000000", bold = true })
-vim.api.nvim_set_hl(0, "to-future-hi-3", { bg = "#ff8800", fg = "#000000", bold = true })
-vim.api.nvim_set_hl(0, "to-future-hi-4", { bg = "#ff0000", fg = "#000000", bold = true })
+vim.api.nvim_set_hl(0, "to-future-hi-1", { bg = "#ffff00", fg = "#000000", bold = true })
+vim.api.nvim_set_hl(0, "to-future-hi-2", { bg = "#ff8800", fg = "#000000", bold = true })
+vim.api.nvim_set_hl(0, "to-future-hi-3", { bg = "#ff0000", fg = "#000000", bold = true })
+opts = {}
 return {
     "suryansh-dey/to-future.nvim",
     event = "VeryLazy",
-    opts = { all_hl = { "to-future-hi-1", "to-future-hi-2", "to-future-hi-3", "to-future-hi-4", "Comment" } },
+    opts = { all_hl = { "to-future-hi-1", "to-future-hi-2", "to-future-hi-3", "Comment" } },
 }
 ```
--- lua/to-future/init.lua
+++ lua/to-future/init.lua
@@ -1,11 +1,10 @@
 return {
     setup = function(opts)
         if #opts == 0 then
-            vim.api.nvim_set_hl(0, "to-future-hi-1", { bg = "#777777", fg = "#000000", bold = true })
-            vim.api.nvim_set_hl(0, "to-future-hi-2", { bg = "#ffff00", fg = "#000000", bold = true })
-            vim.api.nvim_set_hl(0, "to-future-hi-3", { bg = "#ff8800", fg = "#000000", bold = true })
-            vim.api.nvim_set_hl(0, "to-future-hi-4", { bg = "#ff0000", fg = "#000000", bold = true })
-            opts = { all_hl = { "to-future-hi-1", "to-future-hi-2", "to-future-hi-3", "to-future-hi-4", "Comment" } }
+            vim.api.nvim_set_hl(0, "to-future-hi-1", { bg = "#ffff00", fg = "#000000", bold = true })
+            vim.api.nvim_set_hl(0, "to-future-hi-2", { bg = "#ff8800", fg = "#000000", bold = true })
+            vim.api.nvim_set_hl(0, "to-future-hi-3", { bg = "#ff0000", fg = "#000000", bold = true })
+            opts = { all_hl = { "to-future-hi-1", "to-future-hi-2", "to-future-hi-3", "Comment" } }
         end
 
         local utils = require('to-future.utils')
